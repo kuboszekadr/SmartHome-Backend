48ca5996e4dd95b6d899da1ef4331a4f94cb8742
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-04-25 18:26:58 +0200

    Added data collector blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     from . import get_date
+    from . import data_collector
+
     app.register_blueprint(get_date.bp)
+    app.register_blueprint(data_collector.bp)
 
     return app
 
