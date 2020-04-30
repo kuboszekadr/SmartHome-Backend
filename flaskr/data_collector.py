907cc594c96a8f0c539d305c69588931b054de6f
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-04-30 06:07:56 +0200

    Changes to handle new json format

--- flaskr/data_collector.py
+++ flaskr/data_collector.py
@@ -5,21 +5,35 @@ from model import db, Reading
 
 bp = Blueprint('data_collector', __name__)
 
+
 @bp.route('/data_collector', methods=['POST'])
 def data_collector():
     # make sure that only post requests are handled
     assert request.method == 'POST'
 
     data = request.form['data']
-    buf = json.loads(data) # serialize json
+    sensors_data = json.loads(data)  # serialize json
 
-    #insert data into db
-    for b in buf:
-        reading = Reading(sensor_id=b['id'],
-            reading_value=b['value'],
-            reading_measure=1, # TODO
-            reading_timestamp=b['ts'])
-        db.session.add(reading)
+    # stage sensor data
+    for sensor_data in sensors_data:
+        add_sensor_data_to_session(sensor_data)
     db.session.commit()
 
     return "200"
+
+
+def add_sensor_data_to_session(sensor_data):
+    sensor_id = sensor_data['sensor_id']
+    timestamp = sensor_data['ts']
+    values = sensor_data['reading']
+
+    for value in values:
+        measure_id = value['measure_id']
+        value = value['value']
+
+        reading = Reading(sensor_id=sensor_id,
+                          reading_value=value,
+                          reading_measure=measure_id,
+                          reading_timestamp=timestamp
+                          )
+        db.session.add(reading)
