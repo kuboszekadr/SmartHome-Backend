8f8487024aac4222d9243ea5d2ebe1785278e09f
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-09-21 21:07:15 +0200

    Added device2sensor

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,12 +8,15 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    from . import get_date, data_collector, api
+    from . import get_date, data_collector
+    from .api import date, chart_data
     from model import db
 
     app.register_blueprint(get_date.bp)
     app.register_blueprint(data_collector.bp)
-    app.register_blueprint(api.bp)
+    app.register_blueprint(date.bp)
+    app.register_blueprint(chart_data.bp)
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5434/smart_home'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- model.py
+++ model.py
@@ -1,17 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects import postgresql
 
 db = SQLAlchemy()
 
+
 class Measure(db.Model):
-    __tablename__ = 'd_measure'
+    __tablename__ = "d_measure"
     measure_id = db.Column(db.Integer(), primary_key=True)
-    measire_name = db.Column(db.String(255))
+    measure_name = db.Column(db.String(255))
 
 
 class Sensor(db.Model):
-    __tablename__ = 'd_sensor'
+    __tablename__ = "d_sensor"
     sensor_id = db.Column(db.Integer(), primary_key=True)
     sensor_name = db.Column(db.String(255))
+
+
+class DeviceSensor(db.Model):
+    __tablename = "device_sensor"
+
+    device_id = db.Column(db.Integer(), primary_key=True)
+    device_name = db.Column(db.String())
+    sensor_id = db.Column(db.Integer(), primary_key=True)
+    sensor_name = db.Column(db.String())
+    sensor_label = db.Column(db.String())
+    measure_id = db.Column(postgresql.ARRAY(db.Integer()))
 
 
 class Reading(db.Model):
@@ -46,8 +59,8 @@ class ProgramRuntime(db.Model):
 
 class FrondEndReading(db.Model):
     __tablename__ = 'reading'
-    db.Model.metadata.schema = 'front_end'    
-    
+    db.Model.metadata.schema = 'front_end'
+
     device_id = db.Column(db.Integer(), primary_key=True)
 
     sensor_id = db.Column(db.Integer(), primary_key=True)
