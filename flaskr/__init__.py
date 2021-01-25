51140226eaa670fbefd8fa6c5769490d6129376b
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2021-01-25 21:27:29 +0100

    Modified data_collector to use more descriptive json format

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,12 +8,14 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    from . import get_date, data_collector
-    from .api import date, chart_data
+    from . import get_date
+    from . import data_collector as dc
+    from .api import date, chart_data, data_collector
     from model import db
 
     app.register_blueprint(get_date.bp)
     app.register_blueprint(data_collector.bp)
+    app.register_blueprint(dc.bp)
     app.register_blueprint(date.bp)
     app.register_blueprint(chart_data.bp)
     
---
+++ flaskr/api/data_collector.py
@@ -0,0 +1,63 @@
+import json
+
+from flask import Blueprint, request
+from model import db, Reading
+
+bp = Blueprint('api_data_collector', __name__)
+
+
+@bp.route("/api/data_collector", methods=["POST"])
+def data_collector():
+    """
+    Endpoint for data collection from multiple sensors
+
+    Expected data format:
+    {"device_id": 1,
+     "data": {
+         {"sensor_id":1,
+         "readings":["measure_id":1:, "value":22.22],
+         "timestamp":"20200513 063312"
+         }
+     }
+    }
+    """
+    # make sure that only post requests are handled
+    assert request.method == 'POST'
+
+    # Parse json request (sent as form)
+    data_raw = request.form['data']
+    data = json.loads(data_raw)
+    device_id = data['device_id']
+
+    # loop through all available messages
+    for d in data['data']:
+        stage_sensor_readings(d, device_id)
+
+    db.session.commit()  # commit the latest changes
+
+    return '200'
+
+
+def stage_sensor_readings(sensor_data: dict, device_id: int):
+    """
+    Creates new row to be added into readings table
+
+    @param sensor_data: data obtained from the device sensor
+    @param device_id: device sending the data
+    """
+    sensor_id = sensor_data['sensor_id']
+    timestamp = sensor_data['timestamp']
+
+    # one sensor can return multiple measure (for example DHT22)
+    for reading in sensor_data['readings']:
+        measure_id = reading['measure_id']
+        value = reading['value']
+
+        # prepare new row
+        r = Reading(sensor_id=sensor_id,
+                    device_id=device_id,
+                    reading_value=value,
+                    measure_id=measure_id,
+                    reading_timestamp=timestamp
+                    )
+        db.session.add(r)  # stage new program entry
