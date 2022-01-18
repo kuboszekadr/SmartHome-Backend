d7da4249553a44f94d97b3f4d91afb8db8648754
Adrian Kuboszek <adrian.kuboszek@avanade.com>
2022-01-18 09:31:35 +0100

    Gmail notifier - initial commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,12 +11,14 @@ def create_app(test_config=None):
 
     from . import get_date
     from .api import date, chart_data, data_collector, logs
+    from .notifier import notifier
 
     app.register_blueprint(get_date.bp)
     app.register_blueprint(data_collector.bp)
     app.register_blueprint(date.bp)
     app.register_blueprint(chart_data.bp)
     app.register_blueprint(logs.bp)
+    app.register_blueprint(notifier.bp)
     
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{pwd}@{ip}:{port}/{db}'.format(
         user=os.environ['DBUSER'],
