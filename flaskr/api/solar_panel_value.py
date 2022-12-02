ebaacb921743fdfa5d595e4ba442be58f487426b
Adrian Kuboszek <kuboszekadr@gmail.com>
2022-12-02 23:08:50 +0100

    Removed bug when query returns empty set, updated requirements

--- flaskr/api/solar_panel_value.py
+++ flaskr/api/solar_panel_value.py
@@ -1,8 +1,9 @@
 from model import db
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from model.public.queries.queries import get_solarpanel_value_stmt
 
 bp = Blueprint('solar_panel', __name__)
+DEFAULT_WINDOW = 60
 
 @bp.route("/api/v2.0/solar_panel_value", methods=["GET"])
 def get_solar_panel_value():
@@ -11,9 +12,19 @@ def get_solar_panel_value():
     """
 
     r: dict = request.get_json()
-    window = 60
+    window = r.get('window', DEFAULT_WINDOW) if r else DEFAULT_WINDOW
 
     stmt = get_solarpanel_value_stmt(window)[0]
-    result = db.session.execute(stmt)
+    query_result = db.session.execute(stmt)
+    
+    value = query_result.fetchone()[0]
+    
+    value = float(value if value else 0)
+    value = round(value, 0)
 
-    return result
+    result = {
+        'value': value,
+        'window': window,
+    }
+
+    return jsonify(result), 200
